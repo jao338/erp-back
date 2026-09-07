413ea6723e2107bc9cfb0b4eda84a87a8dc5867b
João Henrique <joaohenriquerc123@gmail.com>
2026-09-07 15:27:46 -0300

    chore: Remove comentários desnecessários na criação de tabelas e ajusta rollback para `otp_codes`

--- database/migrations/0001_01_01_000000_create_user_table.php
+++ database/migrations/0001_01_01_000000_create_user_table.php
@@ -20,15 +20,9 @@ return new class extends Migration
             $table->timestamp('verify_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
-
             $table->index('uuid');
             $table->index('email');
         });
-
-        // Apenas após o primeiro acesso com sucesso que o usuário terá acesso ao sistema, o login DEVE barrar um usuário sem acesso
-        // Adicionar attemps via middleware, proteger e impedir enviar múltiplas requisições, 1 a cada 10 minutos
-        // Job depois de 24 horas, deleta os otps
-        //  Testar
 
         Schema::create('otp_codes', function (Blueprint $table) {
             $table->id();
@@ -67,6 +61,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('user');
+        Schema::dropIfExists('otp_codes');
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
